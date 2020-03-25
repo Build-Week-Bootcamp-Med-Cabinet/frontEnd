ea1c8b411d09c2f88426499729b5d21cbc04d0d5
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-03-24 17:14:37 -0700

    Created axiosWithAuth, PrivateRoute, userContext and imported into App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import axiosWithAuth from './utils/axiosWithAuth';
+import userContext from './contexts/index';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
