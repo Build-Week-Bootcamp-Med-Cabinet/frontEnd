03700aa57e90d5ab88c67aecbfeb927a38414eb2
Darrell-Conklin <dmconklin84@gmail.com>
2020-03-25 19:20:32 -0500

    added switch component and default redirect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Header, Home, ProductList } from './components';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Header />
-    
-    <Route exact path="/" component={Home} />
-    <Route path="/productlist" component={ProductList} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/productlist" component={ProductList} />
+    </Switch>
     </>
   );
 }
